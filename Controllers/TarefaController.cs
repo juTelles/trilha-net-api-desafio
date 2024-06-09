838e357ee1b756948b13af942a2d8fd48394590d
juliana <julianatelles.contato@gmail.com>
2024-06-08 21:39:43 -0300

    TODO: Buscar o Id no banco utilizando o EF

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,7 +18,7 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
+            var tarefa = _context.Tarefas.Find(id);
             // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
             // caso contrário retornar OK com a tarefa encontrada
             return Ok();
